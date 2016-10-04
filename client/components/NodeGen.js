7140622c649da7b23bbbb3ee0ec7ff17dc1399a8
ychao21 <changingtimes@gmail.com>
2016-10-03 21:19:30 -0500

    Generates different node shapes depending on whether an invocation or definition. Attached double click handlers so we know which node was toggled. Will need to refactor down the road.

--- client/components/NodeGen.js
+++ client/components/NodeGen.js
@@ -2,28 +2,57 @@
 let SingleNode = function(project, node){
     let startPoint = node.id * 25
 
-    if(node.type === 'invocation'){
-        this.rectangle = new project.Rectangle(new project.Point(25, startPoint), new project.Point(150, (20 + startPoint)));
+    if(node.type === 'definition'){
+        this.rectangle = new project.Rectangle(new project.Point(187.5, startPoint), new project.Point(312.5, (20 + startPoint)));
+
         this.path = new project.Path.Rectangle(this.rectangle);
+
         this.path.strokeColor = 'red';
+        this.path.fillColor = 'red';
+        this.path.secretStorage = node.name
+        this.path.onDoubleClick = function(event){
+            console.log('a definition node got doubleclicked')
+            console.log(this.secretStorage)
+        }
         // Text Example:
-        this.text = new project.PointText(new project.Point(80, 10+startPoint));
+        this.text = new project.PointText(new project.Point(220, 10+startPoint));
         this.text.fillColor = 'black';
         // Set the content of the text item:
         this.text.content = node.name;
+        //adding Doubleclick Event handler
+
+        this.text.onDoubleClick = function(event){
+            console.log('a definition text node got doubleclicked')
+            console.log(this.content)
+        }
+
     }
-    else if(node.type === 'definition'){
+    else if(node.type === 'invocation'){
         this.circle = new project.Path.Circle({
-                center: [87.5, 10 + startPoint],
+                center: [250, 10 + startPoint],
                 radius: 10,
-                strokeColor: 'green'
+                strokeColor: 'green',
+                fillColor: 'blue'
             }
         )
+        //we need to have a fillColor so we can listen to the double click event - Yi
+        this.circle.secretStorage = node.name
 
-        this.circle.strokeColor = 'green';
-        this.text = new project.PointText(new project.Point(80, 10+startPoint));
+        this.text = new project.PointText(new project.Point(230, 10+startPoint));
         this.text.fillColor = 'black';
+
         this.text.content = node.name;
+
+        //attaching double click event handler
+        this.circle.onDoubleClick = function(event){
+            console.log('an invocation node got doubleclicked')
+            console.log(this.secretStorage)
+        }
+        this.text.onDoubleClick = function(event){
+            console.log('an invocation text node got doubleclicked')
+            console.log(this.content)
+        }
+
 
     }
     else{
