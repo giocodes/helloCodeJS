dbf3134eeef2a3166a1ffec459e8ff4edf9a878c
[Gio] <[sigronline@gmail.com]>
2016-10-10 18:19:18 -0500

    added router to load repo URL directly

--- client/components/RepoSelector.js
+++ client/components/RepoSelector.js
@@ -2,14 +2,25 @@
 import React from 'react';
 
 const RepoSelector = React.createClass({
+  componentDidMount(){
+    console.log('did update')
+    if (this.props.location.query.repo){
+      console.log(this.props.location.query);
+    this.fetchRepo(this.props.location.query.repo);
+    // window.document.getElementById('repo-title').innerHTML = "Repo"
+    }
+  },
 
   handleSubmit(e){
     e.preventDefault();
 
-    const repo = this.refs.repo.value;
-    this.props.requestRepoContents(repo);
+    let repo = this.refs.repo.value;
+    this.fetchRepo(repo);
+  },
+  fetchRepo(repoUrl){
+    this.props.requestRepoContents(repoUrl);
     this.props.toggleLoading(true);
-    fetch('http://localhost:7770/api/repo?url=' + repo)
+    fetch('http://localhost:7770/api/repo?url=' + repoUrl)
     .then(response => {
       return response.json();
     })
@@ -22,12 +33,17 @@ const RepoSelector = React.createClass({
 
   render() {
     return (
+      <div>
+      <h2 id="repo-title"></h2>
       <form ref="usernameForm" onSubmit={this.handleSubmit}>
         <div className="form-group">
           <input autoComplete="off" id="github-repo" className="form-control" type="text" ref="repo" placeholder="Enter Github repo URL"/>
           <input type="submit" hidden />
         </div>
       </form>
+        
+      </div>
+
     )
   }
 })
